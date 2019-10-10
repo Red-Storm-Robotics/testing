1221c6f752b4ff0b1871f9bd34aa076f8b5f6f0b
Wesley Chalmers <wesleyjchalmers@gmail.com>
2019-10-09 21:31:11 -0400

    still testing

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,11 @@ void on_center_button() {
     static bool pressed = false;
     pressed = !pressed;
     if (pressed) {
+        pros::lcd::clear_line(2);
         pros::lcd::set_text(2, "I was pressed!");
     } else {
         pros::lcd::clear_line(2);
+        pros::lcd::set_text(2, "Pressed again!");
     }
 }
 
